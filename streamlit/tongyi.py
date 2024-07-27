8e3dfeffadb66d8fe9e4bba816acd598009c0180
Voyagergle <ouyangzhigang@live.com>
2024-07-27 23:38:09 +0800

    fix env

--- streamlit/tongyi.py
+++ streamlit/tongyi.py
@@ -2,12 +2,12 @@ import os
 from langchain_community.llms.tongyi import Tongyi
 from langchain_community.chat_models.tongyi import ChatTongyi
 
-os.environ['TONGYI_KEY'] = 'sk-a70f6e7008314650acf4388068e04490'
+# os.environ['TONGYI_KEY']
 # print(os.environ.get('TONGYI_KEY'))
 
 tongyi_model = Tongyi(
     model_name='qwen-max',
-    dashscope_api_key='sk-a70f6e7008314650acf4388068e04490',
+    dashscope_api_key=os.environ['TONGYI_KEY'],
     stream=True,
     model_kwargs={'temperature': 0.03}
 )
