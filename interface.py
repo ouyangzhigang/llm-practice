d650ce3e947bda85bfb93740c6a5603177d9345d
Voyagergle <ouyangzhigang@live.com>
2024-07-28 11:18:17 +0800

    adjust

--- interface.py
+++
@@ -1,7 +0,0 @@
-from langchain_openai import ChatOpenAI
-from langchain_community.llms.tongyi import Tongyi
-
-# model = ChatOpenAI(model_name='')
-model = ChatOpenAI(model_name='')
-
-model.invoke('hello')
